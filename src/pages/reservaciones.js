80cced3eba65fc511bc8ff8ba318ae71ddef1084
rick-as <rick@aftershock.agency>
2023-11-28 00:14:33 -0500

    feat: add seo deata to reservations

--- src/pages/reservaciones.js
+++ src/pages/reservaciones.js
@@ -2,21 +2,21 @@ import React from "react"
 import Layout from "../components/layout"
 import ReservationsWidget from "../components/reservations-widget/reservations-widget.component"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import SEO from "../components/seo/seo.component"
 
 const queryClient = new QueryClient()
 
 const Reservations = () => {
   return (
-    <Layout
-      seo={{
-        title: "Reservaciones | Manqa ",
-        metaDesc: "Reserva una mesa en Manqa",
-        metaRobotsNoindex: "index",
-        metaRobotsNofollow: "follow",
-      }}
-      hideFooter
-      hideHeader
-    >
+    <Layout hideFooter hideHeader>
+      <SEO
+        data={{
+          title: "Reservaciones | Manqa ",
+          metaDesc: "Reserva una mesa en Manqa",
+          metaRobotsNoindex: "index",
+          metaRobotsNofollow: "follow",
+        }}
+      />
       <QueryClientProvider client={queryClient}>
         <ReservationsWidget />
       </QueryClientProvider>
